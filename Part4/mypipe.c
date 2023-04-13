cee7700393d294de60a1e93ab72396f7fc142834
Yairderry <derry.yair@gmail.com>
2023-04-13 15:28:24 +0300

    Completed part 4

---
+++ Part4/mypipe.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+
+int main(int argc, char const *argv[])
+{
+    char inbuf[100];
+    int p[2], pid, nbytes;
+
+    if (pipe(p) < 0)
+        exit(1);
+
+    if ((pid = fork()) > 0)
+    {
+        printf("parent with pid: %d, Starting to read\n", getpid());
+
+        wait(NULL);
+        if ((nbytes = read(p[0], inbuf, 100)) > 0)
+            printf("%s\nparent with pid: %d, Finished reading\n", inbuf, getpid());
+        if (nbytes < 0)
+            exit(1);
+    }
+    else
+    {
+        printf("child with pid: %d, Starting to write\n", getpid());
+        
+        if ((nbytes = write(p[1], "hello", 100)) > 0)
+            printf("child with pid: %d, Finished writing\n", getpid());
+        if (nbytes < 0)
+            exit(1);
+    }
+    return 0;
+}
